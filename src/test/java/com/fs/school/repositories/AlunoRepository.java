c5d66d059ea0647c9095b2ba6a924579146f3396
Maria Eduarda Scomparin <dudascomparin@gmail.com>
2026-03-02 13:39:48 -0300

    arrumando repository

--- src/test/java/com/fs/school/repositories/AlunoRepository.java
+++ src/test/java/com/fs/school/repositories/AlunoRepository.java
@@ -1,8 +1,9 @@
 package com.fs.school.repositories;
 
+import com.fs.school.models.AlunoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AlunoRepository extends JpaRepository<AlunoRepository, Long> {
+public interface AlunoRepository extends JpaRepository<AlunoModel, Long> {
 }
